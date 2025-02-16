0972a1506df8a9ff1690672a94be041e0df5b81d
Robert Stefanyshin <robertstefanyshin@gmail.com>
2025-02-16 13:14:53 -0600

    Enable mouse in normal and visual mode.

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,7 @@ vim.cmd([[au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe 
 vim.cmd([[nnoremap <leader>ct :!ctags --languages=Scheme,C,C++,Tcl,Lisp,Python -R  --exclude="*obj/*" src<CR>]])
 
 vim.o.showmatch = true -- show matching
-vim.o.mouse = "v" -- middle click paste
+vim.o.mouse = "nv" -- enable mouse in normal and visual mode
 vim.o.hlsearch = true -- highlight search
 vim.o.incsearch = true -- incremental search
 vim.o.tabstop = 4  -- number of columns occupied by a tab
@@ -16,8 +16,4 @@ vim.o.expandtab = true -- convert tabs to whitespace
 vim.o.shiftwidth = 4 -- width for autoindents
 vim.o.autoindent = true -- indent newline same as line above
 vim.o.number = true -- show line numbers
--- vim.o.cc = "80" -- column border at 80
 vim.o.clipboard = "unnamedplus" -- use system clipboard
--- vim.o.colorscheme = "gruvbox" -- set color scheme
--- vim.o.background = "dark" -- background color for gruvbox
-
